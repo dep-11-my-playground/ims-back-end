4f54822428d460d4e939070597c363e4b99c59db
Ruwini-Thakshila <ruwinithakshila2017@gmail.com>
2023-11-22 14:40:02 +0530

    Add TeacherHttpController

---
+++ src/main/java/api/TeacherHttpController.java
@@ -0,0 +1,12 @@
+package api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/teachers")
+@CrossOrigin
+public class TeacherHttpController {
+
+}
